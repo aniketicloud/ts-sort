8be519152c8676eea2d156ab56d9ee9f56450800
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-01-29 14:39:01 +0530

    CharactersCollection sorting

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 import { NumbersCollection } from "./NumbersCollection";
-import { Sorter } from "./Sorter";
 
-const numbersCollection = new NumbersCollection([10, -1, -4, 11]);
-numbersCollection.sort();
-console.log(numbersCollection.data);
+// const numbersCollection = new NumbersCollection([10, -1, -4, 11]);
+// numbersCollection.sort();
+// console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection("Ciao");
-// const stringSorter = new Sorter(charactersCollection);
-// stringSorter.sort();
-
-// console.log(charactersCollection.data);
+const charactersCollection = new CharactersCollection("Ciao");
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 // const linkedList = new LinkedList();
 // linkedList.add(20);
