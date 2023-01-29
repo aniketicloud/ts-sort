34328b218904404ceb0d1dcba4f2f1982de98b1a
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-01-29 14:37:03 +0530

    checking numbersCollection sorting

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,20 @@ import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 
 const numbersCollection = new NumbersCollection([10, -1, -4, 11]);
-const numberSorter = new Sorter(numbersCollection);
-numberSorter.sort();
-// console.log(numbersCollection.data);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection("Ciao");
-const stringSorter = new Sorter(charactersCollection);
-stringSorter.sort();
+// const charactersCollection = new CharactersCollection("Ciao");
+// const stringSorter = new Sorter(charactersCollection);
+// stringSorter.sort();
 
 // console.log(charactersCollection.data);
 
-const linkedList = new LinkedList();
-linkedList.add(20);
-linkedList.add(-2);
-linkedList.add(0);
-linkedList.add(30);
-const linkedListSorter = new Sorter(linkedList);
-linkedListSorter.sort();
-linkedList.print();
+// const linkedList = new LinkedList();
+// linkedList.add(20);
+// linkedList.add(-2);
+// linkedList.add(0);
+// linkedList.add(30);
+// const linkedListSorter = new Sorter(linkedList);
+// linkedListSorter.sort();
+// linkedList.print();
